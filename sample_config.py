04f6141f79f96efa76bb1dfee3b60f15f6f84cb1
akhilsivakosana <76429109+akhilsivakosana@users.noreply.github.com>
2021-03-14 14:03:53 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -1,7 +1,7 @@
-api_id = 14371
-api_hash = "e46b6c854d2bf58a0"
-sudo_chat_id = -1001485876964
-owner_id = 1243703097
+api_id = 2279350
+api_hash = aead80ba4d8b62830a90247b92821a41
+sudo_chat_id = -1001197034850
+owner_id = 1481667840
 
 
 # Arq Api, don't make changes here
